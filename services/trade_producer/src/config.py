87eb6dc877fd8f85f29f2c3f8ad9533a222a493b
Ibrahim Oshinubi <ibrahim.oshinubi@gmail.com>
2024-09-30 17:17:46 +0100

    Extract Config parameter as envinronment variable

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,5 +1,10 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 
+# load .env and access with os.environ[]
+load_dotenv(find_dotenv())
+
+# kafka_broker_address = 'localhost:19092'
 kafka_broker_address = os.environ['KAFKA_BROKER_ADDRESS']
 kafka_topic_name = 'trade'
 product_id = 'BTC/USD'
